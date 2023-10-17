99534078fde740ac052c0a5194b11b1bbc94d835
AMartelanc <alemartelanc@gmail.com>
2023-10-17 10:03:19 -0300

    Final da aula 2 de next | slugs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>HOME PAGE</h1>
+
+      <div>
+        <Link href="/produtos/tenis">TÊNIS</Link> | <Link href="/produtos/produto/tenis-preto">TÊNIS PRETO</Link> | <Link href="/produtos/produto/camisa/camisa-xadrez">CAMISA-X</Link>
+      </div>
     </>
   )
 }
---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Tênis</h1>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempore, ipsam fugit necessitatibus consectetur molestias. Quidem laudantium tempore tenetur doloremque quibusdam animi quisquam ut. Quod eum nihil corporis odio perspiciatis?</p>
+        <p> <Link href="/">Voltar...</Link> </p>
+      </div>
+    </div>
+  )
+}
